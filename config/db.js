67f36eb52ed5ed50cfcf57e5e86734e963158db8
UP220425 <up220425@alumnos.upa.edu.mx>
2025-10-27 14:51:18 -0600

    fix: add mysql dialect for sequelize

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const sequelize = new Sequelize(
@@ -8,9 +9,9 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // 👈 importante para Railway
-    dialect: process.env.DB_DIALECT,
-    logging: false,
+    port: process.env.DB_PORT,
+    dialect: "mysql", // 🔹 ESTA LÍNEA ES LA QUE FALTABA
+    logging: false,   // 🔹 Opcional: quita logs de consola
   }
 );
 
